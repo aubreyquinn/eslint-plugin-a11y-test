49f06247be10968b7848176255562c45c2f17e58
Aubrey Quinn <aubreyquinn@microsoft.com>
2022-10-19 15:35:45 +0100

    got eslint working

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@fluentui/react-components";
+import { CalendarMonthRegular } from '@fluentui/react-icons';
 import * as React from 'react';
 export const App = () => (
     <>
-    <Button>Default</Button>
-    <Button appearance="primary">Primary</Button>
-    <Button appearance="outline">Outline</Button>
-    <Button appearance="subtle">Subtle</Button>
-    <Button appearance="transparent">Transparent</Button>
+      <div id="address_label">Enter your address</div>
+      <Button icon={<CalendarMonthRegular />}></Button>
+      <Button appearance="primary">Primary</Button>
+      <Button appearance="outline">Outline</Button>
+      <Button appearance="subtle">Subtle</Button>
+      <Button appearance="transparent">Transparent</Button>
   </>
 );
