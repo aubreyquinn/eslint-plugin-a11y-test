92ea3bf5151ebe824e505d9362d6d3434b6db653
Aubrey Quinn <aubreyquinn@microsoft.com>
2024-07-16 22:38:40 +0100

    added new test cases in App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { Button, Textarea, Toolbar, ToggleButton } from "@fluentui/react-components";
+import { Button, Textarea, Toolbar, ToggleButton, Label, Combobox, Text } from "@fluentui/react-components";
 import { CalendarMonthRegular } from '@fluentui/react-icons';
 import * as React from 'react';
+const label = "myLabel";
 export const App = () => (
     <>
       <Toolbar />
       <div id="address_label">Enter your address</div>
       <Button icon={<CalendarMonthRegular />}></Button>
+      <Button icon={<CalendarMonthRegular />}>{'Hello'}</Button>
+      <Button icon={<CalendarMonthRegular />}>{label}</Button>
       <ToggleButton icon={<CalendarMonthRegular />}></ToggleButton>
       <Button appearance="primary">Primary</Button>
       <Button appearance="outline">Outline</Button>
@@ -14,5 +17,9 @@ export const App = () => (
       <Button />
       <Textarea />
       <Button icon={<CalendarMonthRegular />} />
+      <Label></Label>
+      <Text></Text>
+      <Combobox />
+      <Button icon={<CalendarMonthRegular />} title="hello"></Button>
   </>
 );
