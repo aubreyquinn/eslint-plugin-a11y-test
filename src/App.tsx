5215b0721259e6eda533e9929deca0dc92aeba92
Aubrey Quinn <aubreyquinn@microsoft.com>
2024-11-07 18:04:38 +0000

    added more conditions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { Button, Textarea, Toolbar, ToggleButton, Label, Combobox, Text } from "@fluentui/react-components";
-import { CalendarMonthRegular } from '@fluentui/react-icons';
+import { Button, Textarea, Toolbar, ToggleButton, Label, Combobox, Text, Input, Badge, Select } from "@fluentui/react-components";
+import { CalendarMonthRegular} from "@fluentui/react-icons";
 import * as React from 'react';
+import { ButtonTests } from "./ButtonTests";
+import { InputTests } from "./InputTests";
 const label = "myLabel";
 export const App = () => (
     <>
+    <ButtonTests />
+    <InputTests />
       <Toolbar />
       <div id="address_label">Enter your address</div>
       <Button icon={<CalendarMonthRegular />}></Button>
@@ -21,5 +25,37 @@ export const App = () => (
       <Text></Text>
       <Combobox />
       <Button icon={<CalendarMonthRegular />} title="hello"></Button>
-  </>
+
+
+      <Input aria-describedby="paragraph_label"></Input>
+      <Label id="paragraph_label">type here</Label>
+
+      <Button aria-describedby="hello"></Button>
+      <p id="hello">type here</p>
+
+      <Badge />
+      <Badge>Abc</Badge>
+      <Badge size="medium" appearance="filled" />
+      <Badge role="img" aria-label="Active" appearance="filled" color="brand" />
+      <Badge appearance="filled" color="brand" />
+      <Badge icon={<CalendarMonthRegular aria-label="paste" />} />
+      <Badge icon={<CalendarMonthRegular />} />
+      <Badge appearance="tint">999+</Badge>
+      <Badge size="medium" icon={<CalendarMonthRegular />} />
+      <Badge size="medium" icon={<CalendarMonthRegular />}>Abc</Badge>
+
+      <Select size="large">
+        <option>Red</option>
+        <option>Green</option>
+        <option>Blue</option>
+      </Select>
+
+      <label htmlFor={`-med`}>Medium</label>
+      <Select id={`-med`} size="medium">
+        <option>Red</option>
+        <option>Green</option>
+        <option>Blue</option>
+      </Select>
+
+  </>      
 );
---
+++ src/ButtonTests.tsx
@@ -0,0 +1,10 @@
+import { Button, Textarea, Toolbar, ToggleButton, Label, Combobox, Text, Input, Badge, Select } from "@fluentui/react-components";
+import { CalendarMonthRegular} from "@fluentui/react-icons";
+import * as React from 'react';
+const label = "myLabel";
+export const ButtonTests = () => (
+    <>
+        <Button></Button>
+        <Button />
+  </>      
+);
